d434e6cb4678c6be7c2cf8812ec8ab95d3d70b55
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-04-07 15:07:39 +0200

    Create antivirus.sh

---
+++ antivirus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update && aptitude safe-upgrade -y && apt install clamav -y
+
+mkdir /root/malwarescan; cd /root/malwarescan; wget http://www.rfxn.com/downloads/maldetect-current.tar.gz; tar xvfz ./maldetect-current.tar.gz;cd maldetect-1.6*;sh install.sh;
+
+wget http://files.f-prot.com/files/unix-trial/fp-Linux.x86.64-fs.tar.gz
+
+tar -xf fp-Linux.*.tar.gz
+cd f-prot/
+./install-f-prot.pl
