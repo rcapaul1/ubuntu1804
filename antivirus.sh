46794eafa7f3e97478e4332dae2f88ef61ec7efc
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-04-07 15:14:39 +0200

    Update antivirus.sh

--- antivirus.sh
+++ antivirus.sh
@@ -4,7 +4,7 @@ apt update && aptitude safe-upgrade -y && apt install clamav -y
 
 mkdir /root/malwarescan; cd /root/malwarescan; wget http://www.rfxn.com/downloads/maldetect-current.tar.gz; tar xvfz ./maldetect-current.tar.gz;cd maldetect-1.6*;sh install.sh;
 
-wget http://files.f-prot.com/files/unix-trial/fp-Linux.x86.64-fs.tar.gz
+wget http://files.f-prot.com/files/unix-trial/fp-Linux.x86.64-ws.tar.gz
 
 tar -xf fp-Linux.*.tar.gz
 cd f-prot/
