504735f4d5bfa6b1f768fc8a2dec6e700213ac97
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-10-14 16:47:39 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-maldet -u -d && maldet -a /root/ && clamscan -fi /root/ && /opt/sophos-av/bin/savupdate && savscan -f wordpress/ -p=/root/savscan.log
+maldet -u -d && maldet -a /root/ && clamscan -fi /root/ && /opt/sophos-av/bin/savupdate && savscan -f /root/ -p=/root/savscan.log
