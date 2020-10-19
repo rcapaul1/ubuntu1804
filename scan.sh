07b28d3523daa345b495019a80ed603f804873bb
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-10-19 15:45:56 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-maldet -u -d && maldet -a /root/ && clamscan -ri /root/ && /opt/sophos-av/bin/savupdate && savscan -f /root/ -p=/root/savscan.log
+maldet -u -d && maldet -a /root/ && clamscan -ri /root/ ; /opt/sophos-av/bin/savupdate && savscan -f /root/ -p=/root/savscan.log
