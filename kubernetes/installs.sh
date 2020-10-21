0c8ddbfd3924722a77cf1d0978370ff718c2aadb
root <root@kubemgmt.capaul.systems>
2020-10-21 10:16:03 +0200

    modified:   kubernetes/installs.sh

--- kubernetes/installs.sh
+++ kubernetes/installs.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# install helm
+apt update && apt install snap zip unzip -y
+snap install helm --classic
+
+# install gscloud
+wget https://github.com/gridscale/gscloud/releases/download/v0.3.0-beta/gscloud_0.3.0-beta_linux_amd64.zip
+unzip gscloud_0.3.0-beta_linux_amd64.zip
+mv gscloud_0.3.0-beta_linux_amd64 /usr/local/bin/gscloud
+gscloud make-config
+
+# edit config
+vi .config/gscloud/config.yaml
 
